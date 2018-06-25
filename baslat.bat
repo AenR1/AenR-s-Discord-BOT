61624c1c1ffcb618e1335e5a30b834463d1e067f
aenr <40569269+aenr@users.noreply.github.com>
2018-06-25 16:14:35 +0300

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,7 @@
+echo off
+color 0f
+title AenR BOT
+cls
+:a
+node bot.js
+goto a
---
+++ bot.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var prefix = '-';
+
+client.on('ready', () => {
+  console.log(`BOT : ${client.user.tag} adı ile giriş yaptı!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'komutlar') {
+    msg.reply('-kurucu');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.reply('Aleyküm Selam Hoşgeldin');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'kurucu') {
+    msg.reply('AenRdır');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'discord.gg') {
+	msg.delete(30)
+    msg.reply('**Reklam engellendi !**');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'temizle') {
+	msg.delete(30)
+    msg.reply('**Chat Temizlendi !**');
+  }
+});
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'lobi');
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle('📤 | Sunucuya Katıldı | Hoşgeldin ')
+  .setTimestamp()
+  channel.sendEmbed(embed);
+});
+
+client.login('NDU5MzM4NDAxNTE1MDQ0ODY1.DhE92Q.o_c_Zst5MagYoZAq_zEuM1PNrrE');
